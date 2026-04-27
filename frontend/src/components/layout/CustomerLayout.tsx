82ad19d8599ac3292104b12efad677889b34bdee
Yassir aittata <yassir.aittata@gmail.com>
2026-04-27 14:21:46 +0100

    create customer layout

---
+++ frontend/src/components/layout/CustomerLayout.tsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom";
+
+const customerLayout = () => {
+  return (
+    <div className="min-h-screen bg-background text-foreground">
+      {/* <navbar /> */}
+      <main className="mx-auto max-w-7xl px-4 py-8">
+        <Outlet />
+      </main>
+    </div>
+  );
+};
+
+export default customerLayout;
